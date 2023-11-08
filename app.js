2c74d33051c0888090db10394e0c51080bf76725
TarikAguen <98916020+TarikAguen@users.noreply.github.com>
2023-11-08 13:45:53 +0100

    emergency reverse initial

--- app.js
+++ app.js
@@ -61,3 +61,68 @@ app.post('/convert', (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
+// Roman to arabic number
+
+app.post('/reverse', (req, res) => {
+  try {
+    const inputRoman = req.body.roman;
+
+    if (!inputRoman) {
+      res.status(400).json({ error: 'Veuillez fournir un chiffre romain dans la requête.' });
+      return;
+    }
+
+    const arabic = romanToArabic(inputRoman);
+    
+    if (arabic === null) {
+      res.status(400).json({ error: 'Chiffre romain invalide.' });
+      return;
+    }
+
+    res.status(200).json({ arabic });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+function romanToArabic(roman) {
+  const romanNumerals = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  let arabic = 0;
+  let currentIndex = 0;
+
+  while (currentIndex < roman.length) {
+    const currentSymbol = roman[currentIndex];
+    const currentSymbolValue = romanNumerals[currentSymbol];
+    const nextSymbol = roman[currentIndex + 1];
+    const nextSymbolValue = romanNumerals[nextSymbol];
+
+    if (nextSymbol && currentSymbolValue < nextSymbolValue) {
+      arabic += nextSymbolValue - currentSymbolValue;
+      currentIndex += 2;
+    } else {
+      arabic += currentSymbolValue;
+      currentIndex += 1;
+    }
+  }
+
+  if (arabicToRoman(arabic) !== roman) {
+    return null;
+  }
+
+  return arabic;
+}
